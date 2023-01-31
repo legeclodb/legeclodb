0178273683f3a50d18e038455e5ddf6d5687bbe1
i-saint <saint.skr@gmail.com>
2023-01-31 14:03:17 +0900

    docs

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? '/legeclodb/' : '/',
   transpileDependencies: true,
-  outputDir: 'dist',
+  outputDir: 'docs',
   assetsDir: './',
   publicPath: './',
 })
