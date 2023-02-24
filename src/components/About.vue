a49cce60993210b3c04f2e1f54d86ea0bdf2c8a4
i-saint <saint.skr@gmail.com>
2023-02-24 14:59:05 +0900

    About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -11,36 +11,78 @@
           <b-link href="https://www.legend-clover.net/" target="_blank">れじぇくろ！</b-link>の攻略データベースです。主にバフ・デバフを検索するのを目的としています。<br />
           間違いの指摘や要望などありましたら以下の公開 Google Docs にコメントいただけると幸いです。<br />
           <b-button variant="secondary" id="btn-docs">Google Docs</b-button><br />
-          あるいは、Twitter で https://legeclodb.github.io/ を含めて発言していただいても拾えると思われます。
         </p>
-        <b-popover target="btn-docs" placement="bottom">
+        <b-popover target="btn-docs" placement="bottom" custom-class="gdocs">
           <iframe width="1100" height="500" src="https://docs.google.com/document/d/1vldiHELiy0GFKhXO20SOsMdw4mczivU4SuxXO9Wv26I/edit?embedded=true"></iframe>
         </b-popover>
 
         <h2>バフ・デバフの競合・重複</h2>
         <p>
           れじぇくろのバフ・デバフの関係は以下のようになっています。<br />
-          ・同系統のバフが同時にかかった場合、アクティブスキル同士は競合し、一番効果が高いもののみ適用。<br />
-          ・ただし、メインキャラのアクティブとサポートキャラのアクティブは別枠で効果が加算になる。<br />
-          ・タレント、パッシブスキル、装備の効果は全て加算。ただし、同スキル・同アイテム同士に限っては競合。<br />
-          ・デバフも同じルールだが上限があり、いくらデバフを重ねても対象の能力は 30% 未満にはならない。<br />
-          ・バフとデバフが同時にかかった場合、% を足し合わせて相殺となる。<br />
+          <ul>
+            <li>同系統のバフが同時にかかった場合、アクティブスキル同士は競合し、一番効果が高いもののみ適用。</li>
+            <li>ただし、メインキャラのアクティブとサポートキャラのアクティブは別枠で効果が加算になる。</li>
+            <li>タレント、パッシブスキル、装備の効果は全て加算。ただし、同スキル・同アイテム同士に限っては競合。</li>
+            <li>デバフも同じルール。バフとデバフが同時にかかった場合、% を足し合わせて相殺となる。</li>
+          </ul>
           <br />
           つまり、だいたい<br />
-          <span style="font-weight: bold;">
-            一番効果の高いメインのアクティブ＋一番効果の高いサポートのアクティブ＋全タレント・パッシブ・装備効果<br />
-          </span>
-          と覚えておいてよいと思われます。<br />
-          これを念頭に置いてバフ・デバフの組み合わせを模索するとよいでしょう。
+          <b>一番効果の高いメインのアクティブ＋一番効果の高いサポートのアクティブ＋全タレント・パッシブ・装備効果</b><br />
+          と覚えておいてよいと思われます。これを念頭にバフ・デバフの組み合わせを模索するとよいでしょう。<br />
+          <br />
+          ただし、与ダメージバフやダメージ耐性デバフはちょっと事情が複雑です。
+          これらは無印、物理、魔法、スキルなどのバリエーションがありますが、<br />
+          異なるバリエーションであればアクティブ同士でも効果が加算になることが確認されています。<br />
+          さらに、一部のスキルは独自のバリエーションになっており、一見競合しそうでも加算になります。<br />
+          <span class="note">例: マリー・アントワネットの<b-link id="id_ロサ・センティフォリア">ロサ・センティフォリア</b-link>など。</span><br />
+          従って、<b-link id="id_総員、突撃用意">総員、突撃用意</b-link>＋<b-link id="id_お届け物です！">お届け物です！</b-link>＋<b-link id="id_リトルマロース">リトルマロース</b-link>＋ロサ・センティフォリア で物理スキル与ダメージ +100% といった事が可能です。<br />
+          <br />
+          一部のバフ・デバフには上限があることが確認されています。<br />
+          該当するのはステータス値へのデバフ (アタック・ディフェンス・マジック・レジスト)、およびダメージ耐性バフと与ダメージデバフで、<br />
+          これらは 70% 以上盛っても実際に効果を発揮するのは 70% までとなります。<br />
+          <br />
+          装備の効果は特に記載がない場合メインのみが対象である点に注意が必要です。<br />
+          サポートも対象の効果は「自ユニットの～」という記載があり、区別されています。<br />
+          <span class="note">例: <b-link id="id_天蠍の天鎧">天蠍の天鎧</b-link>のダメージ耐性はメインのみが対象。<b-link id="id_羅刹のかんざし">羅刹のかんざし</b-link>の物理ダメージ耐性はサポートも対象。</span><br />
+        </p>
+
+        <h2>ダメージ計算</h2>
+        <p>
+          ダメージ計算式は以下のようになっています。<br />
+          <b>(攻撃側の攻撃力－受ける側の防御力)×(スキルのダメージ倍率)×(与ダメージバフ・デバフ)×(ダメージ耐性バフ・デバフ)×(クリティカルダメージ倍率)×乱数×10</b><br />
+          <br />
+          <ul>
+            <li>メイン・サポート個別にこの計算が行われる。</li>
+            <li>攻撃力と防御力は、物理攻撃の場合アタック&ディフェンス、魔法攻撃の場合マジック&レジスト。</li>
+            <li>×10 は 1 戦闘に 10 回攻撃が行われるということで、戦闘アニメーションONだと 1 攻撃あたりのダメージが表示される。</li>
+            <li>
+              ダメージ耐性バフ・デバフと与ダメージバフ・デバフは掛け算の関係。<br />
+              <span class="note">ダメージ耐性バフと与ダメージデバフは 70% が上限で、両方最大まで盛ると 0.3×0.3=9% まで被ダメージを抑え込める。</span>
+            </li>
+            <li>クリティカルダメージ倍率は 30% + バフ効果量で、クリティカル発生時に適用。</li>
+            <li>2回攻撃を持つ場合、最初の一斉攻撃の後、敵が生き残っていたら再度攻撃を行う。</li>
+            <li>範囲攻撃の場合、計算式は同じだが、戦闘の前後に発動するバフ・デバフが発動しないことや、サポートが攻撃に参加しないことが異なる。</li>
+          </ul>
         </p>
       </div>
     </div>
+
+    <template v-for="(item, idx) in popoverItems">
+      <b-popover :target="'id_'+item.name" triggers="hover focus" custom-class="item_po" :title="item.name" placement="top">
+        <div class="flex">
+          <div><b-img-lazy :src="getImageURL(item.name)" width="50" height="50" /></div>
+          <div v-html="descToHtml(item)"></div>
+        </div>
+      </b-popover>
+    </template>
 
   </div>
 </template>
 
 <script>
 import Navigation from './Navigation.vue'
+import jsonMainSkills from '../assets/main_skills.json'
+import jsonItems from '../assets/items.json'
 import common from "./common";
 
 export default {
@@ -53,6 +95,31 @@ export default {
   data() {
     return {
     };
+  },
+
+  mounted() {
+  },
+
+  computed: {
+    popoverItems() {
+      return [
+        this.findMainSkill('総員、突撃用意'),
+        this.findMainSkill('お届け物です！'),
+        this.findMainSkill('リトルマロース'),
+        this.findMainSkill('ロサ・センティフォリア'),
+        this.findItem('天蠍の天鎧'),
+        this.findItem('羅刹のかんざし'),
+      ];
+    },
+  },
+
+  methods: {
+    findMainSkill(name) {
+      return jsonMainSkills.find(a => a.name == name);
+    },
+    findItem(name) {
+      return jsonItems.find(a => a.name == name);
+    }
   }
 }
 </script>
@@ -71,7 +138,27 @@ div.about {
   margin-bottom: 25px;
 }
 
-.popover {
-  max-width: 90% !important;
+.about ul {
+  list-style-type: disc;
+  margin: 0;
+}
+
+.about li {
+  display: list-item;
+  margin: 0 15px;
+}
+
+.gdocs {
+  max-width: 1125px !important;
+  width: 1125px !important;
+}
+
+.item_po {
+  max-width: 400px !important;
+}
+
+.note {
+  font-size: 75%;
+  color: rgb(150, 150, 150);
 }
 </style>
