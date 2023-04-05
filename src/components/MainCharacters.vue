46237abb8fad60d0b9e35b1b7c7ee4ba8e5395d7
i-saint <saint.skr@gmail.com>
2023-04-05 21:20:17 +0900

    攻撃/回復関連タグ

--- src/components/MainCharacters.vue
+++ src/components/MainCharacters.vue
@@ -246,6 +246,11 @@ export default {
           name: "tags_resist",
           tags: new Set(),
         },
+        action: {
+          display: "攻撃/回復",
+          name: "tags_action",
+          tags: new Set(),
+        },
         other: {
           display: "その他",
           name: "tags_other",
@@ -348,6 +353,14 @@ export default {
       };
       handlePredefinedTags(this.tagCategory.buff.tags, ["シンボルスキル"]);
 
+      const isAction = function (t) {
+        for (const n of this.constants.tagsAction) {
+          if (t.startsWith(n))
+            return true;
+        }
+        return false;
+      }.bind(this);
+
       for (let t of this.getMainTags()) {
         if (handledTags.has(t))
           continue;
@@ -358,12 +371,15 @@ export default {
           this.tagCategory.debuff.tags.add(t);
         else if (t.match(/^無効化:/))
           this.tagCategory.resist.tags.add(t);
+        else if (isAction(t))
+          this.tagCategory.action.tags.add(t);
         else
           this.tagCategory.other.tags.add(t);
       }
       this.reorderSet(this.tagCategory.buff.tags, this.constants.tagsBuff);
       this.reorderSet(this.tagCategory.debuff.tags, this.constants.tagsDebuff);
       this.reorderSet(this.tagCategory.resist.tags, this.constants.tagsResist);
+      this.reorderSet(this.tagCategory.action.tags, this.constants.tagsAction);
       this.reorderSet(this.tagCategory.other.tags, this.constants.tagsOther);
       this.reorderSubtag();
     },
